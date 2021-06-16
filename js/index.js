2e6637c9810fcd3d9f6bdb14064e5a82bc2766a8
Justina Kelly <justina@MacBook-Pro-de-Justina.local>
2021-06-16 19:07:53 -0300

    js de index

---
+++ js/index.js
@@ -0,0 +1,100 @@
+alert ("hola")
+
+let urlTrack = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists'
+
+fetch(urlTrack)
+    .then( function(response){
+        return response.json();
+    })
+    .then( function(data){
+
+       console.log(data);
+       let arrayInfo = data.data;
+       let lista = document.querySelector('.ul-de-songs');
+       let contenidoLista =''; 
+
+       for(let i=0; i<arrayInfo.length; i++){
+          contenidoLista += 
+                 `<li>
+                <img src="${arrayInfo[i].album.picture_big}"> 
+                <div class="text"> 
+                    <h5><a href="detail-artist.html?id=${arrayInfo[i].id}"> ${arrayInfo[i].title}</a> </h5>
+                    <p><a href="detail-artist.html?id=${arrayInfo[i].id}"> - ${arrayInfo[i].artist.name}</a></p>
+               </div>
+               </li>`
+       }
+
+      lista.innerHTML += contenidoLista;
+
+
+    })
+    .catch( function(error){
+      console.log(error);
+    })
+
+    let urlAlbum = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/albums'
+
+    fetch(urlAlbum)
+    .then( function(response){
+        return response.json();
+    })
+    .then( function(data){
+        
+        console.log(data);
+       let arrayInfo = data.data;
+       let lista = document.querySelector('.ul-de-albums');
+       let contenidoLista =''; 
+
+       for(let i=0; i<arrayInfo.length; i++){
+          contenidoLista += 
+                              `<li>
+                    <img src="${arrayInfo[i].cover_big}">
+                    <div class="text">
+                        <h5><a href="detail-album.html">${arrayInfo[i].title}</a></h5>
+                        <p><a href="detail-artist.html">- ${arrayInfo[i].artist.name}</a></p>
+                    </div>
+                </li>`
+
+       }
+      lista.innerHTML += contenidoLista;
+
+
+    })
+    .catch( function(error){
+      console.log(error);
+    })
+
+
+    let urlArtist = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists'
+
+    fetch(urlArtist)
+    .then( function(response){
+        return response.json();
+    })
+    .then( function(data){
+
+        console.log(data);
+        let arrayInfo = data.data;
+       let lista = document.querySelector('.ul-de-artist');
+       let contenidoLista =''; 
+
+      for(let i=0; i<arrayInfo.length; i++){
+          contenidoLista += 
+                  
+                `<li>
+                    <img src="${arrayInfo[i].artist.cover_big}">
+                    <div class="text">
+                        <h5><a href="detail-artist.html">${arrayInfo[i].artist.name}</a></h5>
+                    </div>
+                </li>`
+
+       }
+   lista.innerHTML += contenidoLista;
+
+
+    })
+    .catch( function(error){
+      console.log(error);
+    })
+
+  
