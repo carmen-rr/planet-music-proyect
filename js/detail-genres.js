934ded1ff78bd834e9c046817faba2e123c2df97
carmen-rr <crodriguezreynoso@udesa.edu.ar>
2021-06-18 12:01:25 -0300

    detail genres

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,3 +1,6 @@
+/*
+FOMULARIO
+
 let formulario = document.querySelector('form');
 let campoBuscar = document.querySelector('[name="search"]');
 let alert = document.querySelector(".alert");
@@ -25,4 +28,57 @@ formulario.addEventListener('submit', function(event){
 campoBuscar.addEventListener('input', function(){
    alert.innerText = '';
    closeIcon.style.display = 'none'
-})
+})*/
+
+
+let = queryString = location.search;
+let = queryStringToObject = new URLSearchParams(queryString);
+let id = queryStringToObject.get('id');
+
+
+let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}/artists`;
+
+let urlGenres = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}`; 
+
+fetch(urlGenres)
+    .then( function(response){
+        return response.json();
+    })
+    .then( function(data){
+        console.log(data)
+
+    let genre = document.querySelector('h1')
+       genre.innerText =  data.name
+    })
+     .catch( function(error){
+            console.log(error);
+    })
+
+    fetch(url)
+    .then( function(response){
+        return response.json();
+    })
+    .then( function(data){
+        console.log(data)
+
+        let arrayInfo = data.data
+        let lista = document.querySelector ('.detail-genres-article')
+        let contenidoLista =''; 
+     
+        for(let i=0; i<4; i++){
+           contenidoLista += 
+                  `<section class="dua-pop">
+                  <ul>
+                      <li> <a href="detail-artist.html?id=${arrayInfo[i].id}"><h2 class="title-pop">${arrayInfo[i].name}</h2></a></li>
+                      <li> <a href="detail-artist.html?id=${arrayInfo[i].id}"><img class="pop-image" src="${arrayInfo[i].picture_big}" alt="Genres Detail"> </a></li>
+                  </ul>
+              </section>`
+        }
+        lista.innerHTML += contenidoLista;
+
+    })
+     .catch( function(error){
+            console.log(error);
+    })
+
+
