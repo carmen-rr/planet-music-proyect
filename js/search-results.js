9e07cead8a000335cfd543eb3f7b42a5dcc11874
Justina Kelly <justina@MacBook-Pro-de-Justina.local>
2021-06-18 12:48:49 -0300

    empece search-results

--- js/search-results.js
+++ js/search-results.js
@@ -3,3 +3,20 @@ window.addEventListener('load', function(){
         //loader.style.display = 'none';
     
 })
+let queryString = location.search;
+let queryStringToObject = new URLSearchParams(queryString);
+queryStringToObject.get('search')
+
+let url = `https://api.deezer.com/search/artist?buscador=${search}`
+
+fetch (url)
+.then( function(response){
+    return response.json();
+})
+.then( function(data){
+    console.log(data)
+    
+})
+.catch( function(error){
+    console.log(error);
+  })
