aeda6d39153426709537306deb0811b688a28c9c
carmen-rr <crodriguezreynoso@udesa.edu.ar>
2021-06-18 11:32:58 -0300

    formulario probando + html formulario + css formulario

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,1 +1,28 @@
-let url 
+let formulario = document.querySelector('form');
+let campoBuscar = document.querySelector('[name="search"]');
+let alert = document.querySelector(".alert");
+let closeIcon = document.querySelector('.closeIcon');
+ 
+ 
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+ 
+    if(campoBuscar.value == ""){
+       //alert('Please, fill in the field');
+       alert.innerText = 'Please, fill in the field...';
+       closeIcon.style.display = 'inline-block'
+    }
+    else if (campoBuscar.value.length < 3){
+       //alert('Please, fill in with more than 3 characters');
+       alert.innerText = 'Please, fill in with more than 3 characters..';
+        closeIcon.style.display = 'inline-block'
+    }
+    else {
+        this.submit ();
+    }
+})
+ 
+campoBuscar.addEventListener('input', function(){
+   alert.innerText = '';
+   closeIcon.style.display = 'none'
+})
