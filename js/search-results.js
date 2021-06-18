d9c7a212439802c9d84e77bc10d95ccd951a1f02
Justina Kelly <justina@MacBook-Pro-de-Justina.local>
2021-06-18 14:36:49 -0300

    search result tracks

--- js/search-results.js
+++ js/search-results.js
@@ -10,9 +10,9 @@ let search = queryStringToObject.get('search')
 let palabraBuscada = document.querySelector('h2')
 let hola = palabraBuscada.innerText += ` '${search}'`;
 
-let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q=${search}`
+let urlArtist = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q=${search}`
 
-fetch (url)
+fetch (urlArtist)
 .then( function(response){
     return response.json();
 })
@@ -21,7 +21,7 @@ fetch (url)
     let arrayInfo = data.data;
     let searchArtist = document.querySelector('.result-artist');
     let resultArtist = '';
-    for (let i=0; i<arrayInfo.length; i++){
+    for (let i=0; i<3; i++){
         resultArtist += `<li>
                            <img src="${arrayInfo[i].picture_big}" alt="Image ${search}"> 
                            <h4><a href="detail-artist.html?id=${arrayInfo[i].id}">${arrayInfo[i].name}</a></h4>
@@ -33,37 +33,28 @@ fetch (url)
     console.log(error);
   })
 
+let urlTrack = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/track?q=${search}`
 
-/*  let queryString = location.search //Caputramso qs
-let queryStringToObject = new URLSearchParams(queryString); //La transformamos en OL
-let aBuscar = queryStringToObject.get('milanesa'); //Acá va el name del campo input del formulario.
-
-let datoBuscado = document.querySelector('.datoBuscado');
-datoBuscado.innerText = aBuscar;
-
-let url = `https://api.giphy.com/v1/gifs/search?api_key=PuhlljnIs04eW2ezoSHpJ6Fov6102e4T&q=${aBuscar}&limit=25&offset=0&rating=g&lang=en`
-
-fetch( url )
-    .then( function(response){
-        return response.json();
-    })
-    .then( function(data){
-        //Aca muestro código
-        console.log(data);
-        let info = data.data;
-        let section = document.querySelector('.lista');
-        let resultados = '';
-
-        for(let i=0; i<info.length; i++){
-            resultados+= `<article>
-                            <h2>${info[i].title}</h2>
-                            <img src="${info[i].images.original.url}">
-                        </article>`
-        }
-        section.innerHTML += resultados
-        
-
-    })
-    .catch( function(error){
-        console.log(error);
-    }) */
+fetch (urlTrack)
+.then( function(response){
+    return response.json();
+})
+.then( function(data){
+    console.log(data)
+    let arrayInfo = data.data;
+    let searchSongs = document.querySelector('.result-songs');
+    let resultSongs = '';
+    for (let i=0; i<5; i++){
+        resultSongs += `<li>
+        <img src="${arrayInfo[i].album.cover_big}" alt="Portada 'We're Good'">
+        <div>
+          <h4><a href="detail-track.html?id=${arrayInfo[i].id}">We're Good</a></h4>
+          <p><a href="detail-artist.html?id=${arrayInfo[i].artist.id}">${arrayInfo[i].artist.name}</a></p>
+        </div>
+     </li>`
+    }
+    searchSongs.innerHTML += resultSongs;
+})
+.catch( function(error){
+    console.log(error);
+  })
